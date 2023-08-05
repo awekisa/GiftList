9e34e29ce90e99c66554625d1ed54e40e99dea1a
dimitar.valchev <dimitar.valtchev@gmail.com>
2023-08-05 16:33:03 +0300

    implementation

--- client/index.js
+++ client/index.js
@@ -5,10 +5,11 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  let name = process.argv[2];
+  console.log("Input name:", name);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name
   });
 
   console.log({ gift });
